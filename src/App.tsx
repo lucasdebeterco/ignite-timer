a34fa45cc4aa715944476799c5f1537ace02fe42
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-11-27 23:03:23 -0300

    setting up styled-components theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default.ts'
+import { Button } from './styles/components/Button.tsx'
+
 export function App() {
     return (
-        <>a</>
+        <ThemeProvider theme={defaultTheme}>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+        </ThemeProvider>
     )
 }
---
+++ src/styles/components/Button.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
+
+interface IButtonContainer {
+    variant: ButtonVariant
+}
+
+export const ButtonContainer = styled.button<IButtonContainer>`
+    height: 80px;
+    background: ${props => props.theme.primary};
+`
