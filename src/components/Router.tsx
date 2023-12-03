e67e49468afdc72b7936b342342ee8ed1caccdd9
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-12-02 23:58:34 -0300

    using layouts from styled components

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home.tsx'
 import { History } from '../pages/History.tsx'
+import { DefaultLayout } from '../layouts/DefaultLayout/index.tsx'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/history" element={<History />}></Route>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/history" element={<History />}></Route>
+      </Route>
     </Routes>
   )
 }
